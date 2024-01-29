b482f01c69c8a40fb57c8d37796935f7f06b1bd8
bakerhassan <61099003+bakerhassan@users.noreply.github.com>
2024-01-29 13:52:43 -0500

    Update train_CCAM_VOC12_2.py

--- WSSS/train_CCAM_VOC12_2.py
+++ WSSS/train_CCAM_VOC12_2.py
@@ -115,7 +115,6 @@ if __name__ == '__main__':
         transforms.Resize(size=(512, 512)),
         transforms.RandomHorizontalFlip(),
         transforms.RandomCrop(size=(448, 448)),
-        transforms.ToTensor(),
         transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
     ])
 
